225ba9a7fd6b459fe23cc2c44a578de4501eaf4c
Abshir <abshirm@live.com>
2019-07-16 15:56:43 -0700

    initial commit

---
+++ firebase_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase_app
+//
+//  Created by Abshir Mohamed on 7/16/19.
+//  Copyright © 2019 Abshir Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
