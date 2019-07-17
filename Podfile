883e387867b6978cf493e8ec2fa360390602f946
Abshir <abshirm@live.com>
2019-07-16 18:52:54 -0700

    successful connection with the database

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'firebase_app' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for firebase_app
+
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+
+end
--- firebase_app/ViewController.swift
+++ firebase_app/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let ref = Database.database().reference()
+        
+//lines 21 and and 22 add data to DB
+//        ref.child("someid/name").setValue("Mike")
+//        ref.childByAutoId().setValue(["name":"Tom","role":"Admin","age":30])
+        
+//lines 25 - 30 retrieve data from database
+        
+        ref.child("someid/name").observeSingleEvent(of: .value)
+        {
+            //Retrieves one value from specified key
+            (snapshot) in let name = snapshot.value as? String
+
+             print(name)
+    
+//            (snapshot) in let employeedata = snapshot.value as? [String:Any]
+        }
+        
+       
+        
     }
 
 
